22fa00c954940bae897810b0f31bc5c72a73744b
J Parsons <j.andrew.parsons@gmail.com>
2026-06-14 15:06:34 -0700

    Refactor extract module to improve docstrings and implement data loading functions for NEOs and close approaches

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
-"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+"""Extract data on near-Earth objects and close approaches from
+CSV and JSON files.
 
 The `load_neos` function extracts NEO data from a CSV file, formatted as
 described in the project instructions, into a collection of `NearEarthObject`s.
@@ -7,10 +8,8 @@ The `load_approaches` function extracts close approach data from a JSON file,
 formatted as described in the project instructions, into a collection of
 `CloseApproach` objects.
 
-The main module calls these functions with the arguments provided at the command
-line, and uses the resulting collections to build an `NEODatabase`.
-
-You'll edit this file in Task 2.
+The main module calls these functions with the arguments provided at the
+command line, and uses the resulting collections to build an `NEODatabase`.
 """
 import csv
 import json
@@ -21,18 +20,41 @@ from models import NearEarthObject, CloseApproach
 def load_neos(neo_csv_path):
     """Read near-Earth object information from a CSV file.
 
-    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :param neo_csv_path: A path to a CSV file containing data about
+    near-Earth objects.
     :return: A collection of `NearEarthObject`s.
     """
-    # TODO: Load NEO data from the given CSV file.
-    return ()
+    neos = []
+    with open(neo_csv_path, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            neo = NearEarthObject(
+                pdes=row['pdes'],
+                name=row['name'],
+                diameter=row['diameter'] if row['diameter'] else float('nan'),
+                hazardous=row['pha'] == 'Y'
+            )
+            neos.append(neo)
+    return neos
 
 
 def load_approaches(cad_json_path):
     """Read close approach data from a JSON file.
 
-    :param cad_json_path: A path to a JSON file containing data about close approaches.
+    :param cad_json_path: A path to a JSON file containing data about
+    close approaches.
     :return: A collection of `CloseApproach`es.
     """
-    # TODO: Load close approach data from the given JSON file.
-    return ()
+    approaches = []
+    with open(cad_json_path, 'r') as f:
+        reader = json.load(f)
+        fields = reader['fields']
+        data = [dict(zip(fields, item)) for item in reader['data']]
+        for item in data:
+            approach = CloseApproach(
+                cd=item['cd'],
+                dist=item['dist'],
+                v_rel=item['v_rel']
+            )
+            approaches.append(approach)
+    return approaches
